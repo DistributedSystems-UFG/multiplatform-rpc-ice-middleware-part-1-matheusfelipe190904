39266f3a8e6721b3b3a2edefa8321bb3ca312291
EC2 Default User <ec2-user@ip-172-31-95-98.ec2.internal>
2026-04-19 13:56:23 +0000

    Adiciona os metodos countWords e reverseString ao server2 e client2

--- client2.py
+++ client2.py
@@ -1,18 +1,27 @@
 import sys, Ice
 import Demo
- 
-communicator = Ice.initialize(sys.argv)
 
-base1 = communicator.stringToProxy("SimplePrinter1:tcp -h 98.90.53.6 -p 11000")
-base2 = communicator.stringToProxy("SimplePrinter2:tcp -h 98.90.53.6 -p 11000")
+communicator = Ice.initialize(sys.argv)
+base1 = communicator.stringToProxy("SimplePrinter1:tcp -h 3.84.16.92 -p 11000")
+base2 = communicator.stringToProxy("SimplePrinter2:tcp -h 3.84.16.92 -p 11000")
 printer1 = Demo.PrinterPrx.checkedCast(base1)
 printer2 = Demo.PrinterPrx.checkedCast(base2)
 if (not printer1) or (not printer2):
     raise RuntimeError("Invalid proxy")
 
 rep = printer1.printString("Hello World from printer1!")
-print(rep)
+print("printString:", rep)
 rep = printer2.printString("Hello World from printer2!")
-print(rep)
+print("printString:", rep)
+
+count = printer1.countWords("Hello World from ICE middleware")
+print("countWords printer1:", count)
+count = printer2.countWords("Sistemas Distribuidos UFG")
+print("countWords printer2:", count)
+
+rev = printer1.reverseString("Sistemas Distribuidos")
+print("reverseString printer1:", rev)
+rev = printer2.reverseString("Hello World")
+print("reverseString printer2:", rev)
 
 communicator.waitForShutdown()
--- server2.py
+++ server2.py
@@ -1,21 +1,29 @@
 import sys, Ice
 import Demo
- 
+
 class PrinterI(Demo.Printer):
     def __init__(self, t):
         self.t = t
-        
+
     def printString(self, s, current=None):
         print(self.t, s)
         return s + "*"
 
-communicator = Ice.initialize(sys.argv) 
+    def countWords(self, s, current=None):
+        count = len(s.split())
+        print(self.t, f"countWords('{s}') = {count}")
+        return count
 
+    def reverseString(self, s, current=None):
+        result = s[::-1]
+        print(self.t, f"reverseString('{s}') = '{result}'")
+        return result
+
+communicator = Ice.initialize(sys.argv)
 adapter = communicator.createObjectAdapterWithEndpoints("SimpleAdapter", "default -p 11000")
 object1 = PrinterI("Object1 says:")
 object2 = PrinterI("Object2 says:")
 adapter.add(object1, communicator.stringToIdentity("SimplePrinter1"))
 adapter.add(object2, communicator.stringToIdentity("SimplePrinter2"))
 adapter.activate()
-
 communicator.waitForShutdown()
